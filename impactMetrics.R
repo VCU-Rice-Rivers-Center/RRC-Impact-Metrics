d1fe8dc8f58187e50d9a34cff9799872feb5578b
laurenkwick <laurenkwick@gmail.com>
2026-08-17 16:17:33 -0400

    Calculate columns for personnel estimates

--- impactMetrics.R
+++ impactMetrics.R
@@ -1,7 +1,12 @@
 # Load packages
 library(googlesheets4)
+library(dplyr)
+library(lubridate)
+library(strex)
 
-# Read the RRC Reservation Form response spreadsheet
+######################
+### Authentication ###
+######################
 
 # Fetch the key from the system environment
 gcp_key <- Sys.getenv("GCP_SA_KEY")
@@ -16,5 +21,42 @@ if (nzchar(gcp_key)) {
   message("Authenticated via browser.")
 }
 
+#################
+### Read Data ###
+#################
+
+# Sheet ID for reservation request form responses:
 sheet_id <- "1ed3zy3QERXUd7yMlfxtwxqAu3sqhpNWP-TScr1Y_dMM"
 reservations <- read_sheet(sheet_id)
+
+##################
+### Processing ###
+##################
+
+# Formatting free text columns from list -> character
+reservations$`How many Non-VCU affiliated individuals will there be?` <- as.character(reservations$`How many Non-VCU affiliated individuals will there be?`)
+reservations$`How many VCU employees will there be?` <- as.character(reservations$`How many VCU employees will there be?`)
+reservations$`How many VCU students will there be?` <- as.character(reservations$`How many VCU students will there be?`)
+
+
+# Calculate additional columns:
+reservations_v2 <- reservations %>%
+  mutate(
+    # Calculate number of days requested
+    length_of_stay_days = as.numeric(difftime(`Requested departure date`, `Requested arrival date`, units = "days")) + 1,
+    
+    # Extract year
+    year = year(`Requested arrival date`),
+    
+    # Approximate count of VCU students from free text response
+    count_vcu_students_low = str_first_number(`How many VCU students will there be?`),
+    count_vcu_students_high = str_last_number(`How many VCU students will there be?`),
+    
+    # Approximate count of VCU staff/faculty from free text response
+    count_vcu_staff_low = str_first_number(`How many VCU employees will there be?`),
+    count_vcu_staff_high = str_last_number(`How many VCU employees will there be?`),
+    
+    # Approximate count of non-VCU persons from free text response
+    count_non_vcu_low = str_first_number(`How many Non-VCU affiliated individuals will there be?`),
+    count_non_vcu_high = str_last_number(`How many Non-VCU affiliated individuals will there be?`)
+    )
